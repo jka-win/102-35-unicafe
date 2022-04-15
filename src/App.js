482d6f3153bba4d635064f2661dca72bd908799f
Joseph Anderson <josepha93@gmail.com>
2022-04-15 13:51:24 -0400

    step3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const total = good + neutral + bad;
 
   return (
     <div>
@@ -14,19 +13,29 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
 
-      <h1>statistics</h1>
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />      
-      <Statistic text="bad" value={bad} />
-      <p>all {total}</p>
-      <p>average {(good - bad) / total}</p>
-      <p>positive {good / total * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
 
 const Button = props => 
   <button onClick={props.onClick}>{props.text}</button>;
+
+const Statistics = props => {
+  const total = props.good + props.neutral + props.bad;
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <Statistic text="good" value={props.good} />
+      <Statistic text="neutral" value={props.neutral} />      
+      <Statistic text="bad" value={props.bad} />
+      <p>all {total}</p>
+      <p>average {(props.good - props.bad) / total}</p>
+      <p>positive {props.good / total * 100} %</p>
+    </div>
+  );
+};
 
 const Statistic = props =>
   <p>{props.text} {props.value}</p>;
